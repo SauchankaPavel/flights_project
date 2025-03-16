15174076e124c7e3196ff9a210747bbae45aeb42
p.sauchanka <p.sauchanka@andersenlab.com>
2025-03-16 22:21:31 +0100

    feature: optimize subscriptions

--- src/app/pages/flight/flight.component.ts
+++ src/app/pages/flight/flight.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { FlightItem, FlightsService } from '../../services/flights.service';
 import { FilterFormState } from './flight/filters/filters.component';
+import { BehaviorSubject, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-flight',
@@ -9,34 +10,45 @@ import { FilterFormState } from './flight/filters/filters.component';
   templateUrl: './flight.component.html',
   styleUrl: './flight.component.scss'
 })
-export class FlightComponent implements OnInit{
+export class FlightComponent implements OnInit, OnDestroy{
   currentflightItems: FlightItem[] = []
   pageSize: number = 5;
   pageIndex: number = 0;
   formValue: FilterFormState | undefined
+
+  private destroy$ = new Subject<void>();
+  private flightParams$ = new BehaviorSubject<{ pageSize: number; pageIndex: number; formValue?: FilterFormState }>({
+    pageSize: this.pageSize,
+    pageIndex: this.pageIndex
+  });
   constructor(private flightService: FlightsService){
 
   }
 
   ngOnInit(): void {
-    this.flightService.getFlights(this.pageSize, this.pageIndex).subscribe((res)=>{
-      this.currentflightItems = res
-    })
+    this.flightParams$
+      .pipe(
+        switchMap(params => this.flightService.getFlights(params.pageSize, params.pageIndex, params.formValue)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(res => {
+        this.currentflightItems = res;
+      });
   }
 
   filterFormChangeHandler(formValue: FilterFormState){
-    this.pageSize = 5
-    this.formValue = formValue
-    this.flightService.getFlights(this.pageSize, this.pageIndex, formValue).subscribe((res)=>{
-      this.currentflightItems = res
-    })
+    this.formValue = formValue;
+    this.flightParams$.next({ pageSize: 5, pageIndex: this.pageIndex, formValue });
   }
 
   showMoreHandler(){
-    this.pageSize += 5
-    this.flightService.getFlights(this.pageSize, this.pageIndex, this.formValue).subscribe((res)=>{
-      this.currentflightItems = res
-    })
+    this.pageSize += 5;
+    this.flightParams$.next({ pageSize: this.pageSize, pageIndex: this.pageIndex, formValue: this.formValue });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
 
--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -9,69 +9,71 @@ import { FormState } from "../pages/traveler/traveler.component";
 })
 
 export class FlightsService {
-    baseUrl: string = "https://public-front-bucket.s3.eu-central-1.amazonaws.com/test/test_flights.json"
-    private selectedFlight: FlightItem | undefined;
-    private formState: FormState | undefined;
-    totalCountOfItems = 0;
-    constructor(private http: HttpClient){
-  
-    }
-    getFlights( pageSize: number, pageIndex: number, formValue?: FilterFormState,): Observable<FlightItem[]>{
-        return this.http.get<FlightItem[]>(this.baseUrl).pipe(
-            map((res: FlightItem[]) => {
-              let mappedResult = res.map(
-                (result) => {
-                  return {
-                    flights: result.flights,
-                    id: result.id,
-                    price: result.price,
-                    totalStops: result.flights.reduce((total, flight) => total + flight.stops, 0)
-                  };
-                }
-              );
-              //filter by price range
-              if(formValue?.priceRangeEnd){
-                mappedResult =  mappedResult.filter(item => item.price >= formValue?.priceRangeStart && item.price <= formValue.priceRangeEnd);
-                
-              }
-              //filter by stops
-              if(formValue?.stops&&formValue?.stops>0){
-                mappedResult = mappedResult.filter(item => item.totalStops === formValue.stops-1);
-              }
-              //sort
-              if (formValue?.sorting){
-                if(formValue.sorting==="Price (Lowest)"){
-                  mappedResult.sort((a, b) => a.price - b.price);
-                }else if(formValue.sorting==="Price (Highest)"){
-                  mappedResult.sort((a, b) => b.price - a.price);
-                }
-              }
-              return mappedResult
-            })
-        )
-        .pipe( map((data) => {
-          this.totalCountOfItems = data.length
-          const startIndex = pageIndex * pageSize;
-          const endIndex = startIndex + pageSize;
-          return data.slice(startIndex, endIndex);
+  private baseUrl: string = 'https://public-front-bucket.s3.eu-central-1.amazonaws.com/test/test_flights.json';
+  private selectedFlight: FlightItem | undefined;
+  private formState: FormState | undefined;
+  totalCountOfItems = 0;
+
+  constructor(private http: HttpClient) {}
+
+  getFlights(
+    pageSize: number,
+    pageIndex: number,
+    formValue?: FilterFormState
+  ): Observable<FlightItem[]> {
+    return this.http.get<FlightItem[]>(this.baseUrl).pipe(
+      map((res: FlightItem[]) => {
+        let mappedResult = res.map((result) => ({
+          flights: result.flights,
+          id: result.id,
+          price: result.price,
+          totalStops: result.flights.reduce((total, flight) => total + flight.stops, 0),
         }));
-    }
 
-    setFlight(flight: FlightItem) {
-      this.selectedFlight = flight;
-    }
-  
-    getFlight(): FlightItem | undefined {
-      return this.selectedFlight;
-    }
+        // Apply filters based on form values
+        if (formValue) {
+          if (formValue.priceRangeEnd) {
+            mappedResult = mappedResult.filter(
+              (item) => item.price >= formValue.priceRangeStart && item.price <= formValue.priceRangeEnd
+            );
+          }
 
-    setFormState(formState: FormState) {
-      this.formState = formState;
-    }
-  
-    getFormState(): FormState | undefined {
-      return this.formState;
-    }
+          if (formValue.stops && formValue.stops > 0) {
+            mappedResult = mappedResult.filter((item) => item.totalStops === formValue.stops - 1);
+          }
+
+          if (formValue.sorting) {
+            if (formValue.sorting === 'Price (Lowest)') {
+              mappedResult.sort((a, b) => a.price - b.price);
+            } else if (formValue.sorting === 'Price (Highest)') {
+              mappedResult.sort((a, b) => b.price - a.price);
+            }
+          }
+        }
+
+        this.totalCountOfItems = mappedResult.length;
+        const startIndex = pageIndex * pageSize;
+        const endIndex = startIndex + pageSize;
+        return mappedResult.slice(startIndex, endIndex);
+      })
+    );
+  }
+
+  setFlight(flight: FlightItem): void {
+    this.selectedFlight = flight;
+  }
+
+  getFlight(): FlightItem | undefined {
+    return this.selectedFlight;
+  }
+
+  setFormState(formState: FormState): void {
+    this.formState = formState;
+  }
+
+  getFormState(): FormState | undefined {
+    return this.formState;
+  }
 }
 
 export type FlightItem = {
