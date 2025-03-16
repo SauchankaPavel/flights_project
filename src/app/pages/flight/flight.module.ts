5eeee1e992443b15c4ed93056ee1550d442fe8e9
p.sauchanka <p.sauchanka@andersenlab.com>
2025-03-16 17:20:34 +0100

    add angular material and basic markup for filters

--- src/app/pages/flight/flight.module.ts
+++ src/app/pages/flight/flight.module.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { FlightRoutingModule } from './flight-routing.module';
 import { FlightComponent } from './flight.component';
+import { MatInputModule } from '@angular/material/input';
+import { FlightListComponent } from './flight/flight-list/flight-list.component';
+import { FiltersComponent } from './flight/filters/filters.component';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 
 @NgModule({
   declarations: [
-    FlightComponent
+    FlightComponent,
+    FlightListComponent,
+    FiltersComponent
   ],
   imports: [
     CommonModule,
-    FlightRoutingModule
+    FlightRoutingModule,
+    MatInputModule,
+    MatSelect,
+    MatOption,
+    MatSliderModule,
+    MatCheckbox
   ]
 })
 export class FlightModule { }
---
+++ src/app/pages/flight/flight/flight-list/flight-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-list',
+  standalone: false,
+  templateUrl: './flight-list.component.html',
+  styleUrl: './flight-list.component.scss'
+})
+export class FlightListComponent {
+
+}
